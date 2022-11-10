b0b1ac2e72fc219acc4171e5bc068bd2b98e61e7
Lil Juice <58902513+liljuice@users.noreply.github.com>
2022-11-10 23:16:08 +0900

    Update and rename 1989(wip).py to 1966.py

--- 1966.py
+++ 1966.py
@@ -3,18 +3,17 @@ for _ in range(t):
   n, m = map(int, input().split())
   queue = list(map(int, input().split()))
   index = list(range(0, n))  # index : 문서 번호, queue : 문서의 중요도
-
   printOrder = 0
 
-  while True:  # 시행 최대 횟수는 n 이상임..
+  while queue:  # 시행 최대 횟수는 n 이상임..
     # 맨 앞 문서의 중요도 비교 후 재배치
     if queue[0] == max(queue):  # 맨 앞 문서의 중요도가 가장 높은 경우
-      queue.pop()
+      queue.pop(0)
       printOrder += 1
-      if index.pop() == m:
+      if index.pop(0) == m:
         print(printOrder)
         break
 
-    if queue[0] < max(queue):
-      queue.append(queue.pop())
-      index.append(index.pop())
+    else:
+      queue.append(queue.pop(0))
+      index.append(index.pop(0))
